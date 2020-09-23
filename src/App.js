f56d0f677c37f2246802364fd25a037bdbe9bf63
Brett Brennan <brettthederp@gmail.com>
2020-09-23 17:41:11 -0400

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+/*
+  process.env.CLIENT_ID
+  process.env.CLIENT_TOKEN
+  https://api.twitch.tv/helix/search/channels?query=
+*/
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
